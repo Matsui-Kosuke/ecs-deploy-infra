f81040bae1b021b6ee0351ff9bd9a961460a0557
Matsui-Kosuke <91716913+Matsui-Kosuke@users.noreply.github.com>
2023-12-03 22:12:31 +0900

    local Valuesを使って一部共通化

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -1,36 +1,11 @@
-resource "aws_ecr_repository" "nginx" {
-  name                 = "ecs-deploy-prod-nginx"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-resource "aws_ecr_lifecycle_policy" "nginx" {
-  policy = jsonencode(
-    {
-      "rules" : [
-        {
-          "rulePriority" : 1,
-          "description" : "Hold only 10 images, expire all others",
-          "selection" : {
-            "tagStatus" : "any",
-            "countType" : "imageCountMoreThan",
-            "countNumber" : 5
-          },
-          "action" : {
-            "type" : "expire"
-          }
-        }
-      ]
-    }
-  )
-  repository = aws_ecr_repository.nginx.name
-}
-
 module "nginx" {
     source = "../../modules/ecr"
     
-    name = "ecs-deploy-prod-nginx"
+    name = "${local.service_name}-prod-nginx"
+}
+
+module "php" {
+    source = "../../modules/ecr"
+    
+    name = "${local.service_name}-prod-php"
 }
